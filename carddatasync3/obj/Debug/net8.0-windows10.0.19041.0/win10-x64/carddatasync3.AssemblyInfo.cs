47fc7b51e123af6dd2992f3ae31c67511ae447cd
ChiuzuChilumbuRD <chiuzu.chilumbu@metaguru.com.tw>
2024-10-21 13:29:46 +0800

    Database Factory Added

--- carddatasync3/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/carddatasync3.AssemblyInfo.cs
+++ carddatasync3/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/carddatasync3.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("carddatasync3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0c94c43d3b6207d24002b04b3bc8d63cca0ae7ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+cce0ddfbcdea2d86550bd77bab7c864f0b4486f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("carddatasync3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("carddatasync3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
