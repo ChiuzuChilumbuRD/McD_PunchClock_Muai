57a59404f608ed63019c0471b8ba405828426d5b
ChiuzuChilumbuRD <chiuzu.chilumbu@metaguru.com.tw>
2024-10-21 13:58:00 +0800

    [WIP]

--- carddatasync3/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/carddatasync3.AssemblyInfo.cs
+++ carddatasync3/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/carddatasync3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("carddatasync3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+cce0ddfbcdea2d86550bd77bab7c864f0b4486f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+47fc7b51e123af6dd2992f3ae31c67511ae447cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("carddatasync3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("carddatasync3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
