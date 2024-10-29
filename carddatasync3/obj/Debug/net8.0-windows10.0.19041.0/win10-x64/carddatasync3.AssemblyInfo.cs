3e98ac0c0239637b31057478898b2de1bc9a491b
ChiuzuChilumbuRD <chiuzu.chilumbu@metaguru.com.tw>
2024-10-29 11:24:11 +0800

    Before pull

--- carddatasync3/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/carddatasync3.AssemblyInfo.cs
+++ carddatasync3/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/carddatasync3.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("carddatasync3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3eb5f7a194f12fb8a99dbab16632862f0044e9b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+cafa14793e4b9452f7a9fe5071d911d8cfac1a10")]
 [assembly: System.Reflection.AssemblyProductAttribute("carddatasync3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("carddatasync3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
